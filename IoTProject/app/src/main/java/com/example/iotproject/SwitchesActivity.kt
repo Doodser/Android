00f963343826dce395a9a78200d28a6e4b033019
Степанов Егор Сергеевич <StepanovES@DOM201.LOCAL>
2024-02-17 17:14:09 +0700

    LightControl1 added synchronization

--- IoTProject/app/src/main/java/com/example/iotproject/SwitchesActivity.kt
+++ IoTProject/app/src/main/java/com/example/iotproject/SwitchesActivity.kt
@@ -10,6 +10,8 @@ import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import java.lang.Exception
 
 class SwitchesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,20 +22,24 @@ class SwitchesActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         val queue = Volley.newRequestQueue(this)
-        val url = "http://84.237.51.142:5000/light_control/switches/states"
+        val url = "http://84.237.51.142:5000/light_control/switches"
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             {response ->
-                val switchesStates = Json.decodeFromString<Map<String, Boolean>>(response)
+                val switchesStates = Json.decodeFromString<Array<JsonObject>>(response)
+
                 val switches = mutableListOf<SwitchCompat>()
+                val switchesNames = mutableListOf<String>()
+
                 for (s in switchesStates) {
                     val switch = SwitchCompat(this)
-                    switch.text = s.key
-                    switch.isChecked = s.value
+                    switch.text = s["id"].toString().replace("\"", "")
+                    switch.isChecked = (s["state"].toString() == "true")
 
                     switches.add(switch)
+                    switchesNames.add(s["name"].toString().replace("\"", ""))
                 }
-                recyclerView.adapter = SwitchesRecyclerAdapter(switches)
+                recyclerView.adapter = SwitchesRecyclerAdapter(switches, switchesNames)
             },
             {e ->
                 e.printStackTrace()
--- IoTProject/app/src/main/java/com/example/iotproject/SwitchesRecyclerAdapter.kt
+++ IoTProject/app/src/main/java/com/example/iotproject/SwitchesRecyclerAdapter.kt
@@ -3,22 +3,26 @@ package com.example.iotproject
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
+import android.widget.TextView
 import androidx.appcompat.widget.SwitchCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.AuthFailureError
 import com.android.volley.Request
-import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonArray
 import org.json.JSONObject
+import java.util.Timer
+import java.util.TimerTask
 
 
-class SwitchesRecyclerAdapter(private val switches: List<SwitchCompat>):
+class SwitchesRecyclerAdapter(private val switches: List<SwitchCompat>,
+                              private val switchesNames: List<String>):
     RecyclerView.Adapter<SwitchesRecyclerAdapter.ViewHolder>(){
+
+    private val timer = Timer()
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val switchName: TextView = itemView.findViewById(R.id.switchName)
         val switch: SwitchCompat = itemView.findViewById(R.id.settingSwitch)
     }
 
@@ -30,31 +34,57 @@ class SwitchesRecyclerAdapter(private val switches: List<SwitchCompat>):
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-       holder.switch.text = switches[position].text
-       holder.switch.isChecked = switches[position].isChecked
+        holder.switchName.text = switchesNames[position]
+        holder.switch.text = switches[position].text
+        holder.switch.isChecked = switches[position].isChecked
 
-       holder.switch.setOnCheckedChangeListener { compoundButton, isChecked ->
-           holder.switch.isChecked = isChecked
+        holder.switch.setOnCheckedChangeListener {compoundButton, isChecked ->
+            holder.switch.isChecked = isChecked
 
-           val queue = Volley.newRequestQueue(holder.switch.context)
-           val url = "http://84.237.51.142:5000/light_control/switches/states"
-           val stringRequest = JsonObjectRequest(
-               Request.Method.POST,
-               url,
-               JSONObject("{${holder.switch.text}:$isChecked}"),
-               {response ->
-                  val responseMap = Json.decodeFromString<Map<String, Boolean>>(response.toString())
-                  if (responseMap[holder.switch.text] != isChecked) {
-                      holder.switch.isChecked = !holder.switch.isChecked
-                  }
-               },
-               {e ->
-                   holder.switch.isChecked = !holder.switch.isChecked
-               }
-           )
+            val queue = Volley.newRequestQueue(holder.switch.context)
+            val url = "http://84.237.51.142:5000/light_control/switches/states"
+            val jsonRequest = JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                JSONObject("{${holder.switch.text}:$isChecked}"),
+                {response ->
+                    val responseMap = Json.decodeFromString<Map<String, Boolean>>(response.toString())
+                    if (responseMap[holder.switch.text] != isChecked) {
+                        holder.switch.isChecked = !holder.switch.isChecked
+                    }
+                },
+                {e ->
+                    holder.switch.isChecked = !holder.switch.isChecked
+                }
+            )
 
-           queue.add(stringRequest)
-       }
+            queue.add(jsonRequest)
+        }
+
+        timer.schedule(
+            object : TimerTask() {
+                override fun run() {
+                    val queue = Volley.newRequestQueue(holder.switch.context)
+                    val url = "http://84.237.51.142:5000/light_control/switches/states"
+                    val jsonRequest = JsonObjectRequest(
+                        url,
+                        {response ->
+                            val responseMap = Json.decodeFromString<Map<String, Boolean>>(response.toString())
+                            if (responseMap[holder.switch.text] != holder.switch.isChecked) {
+                                holder.switch.isChecked = responseMap[holder.switch.text]!!
+                            }
+                        },
+                        {e ->
+                            e.printStackTrace()
+                        }
+                    )
+
+                    queue.add(jsonRequest)
+                }
+            },
+            1000,
+            3500
+        )
 
     }
 
